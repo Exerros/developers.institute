96680cb38758ef4e7423bd371e97696130cf229b
Daniel Sirot <danielsirot@gmail.com>
2023-04-01 18:32:48 +0300

    .

---
+++ week3/day4/DailyChallenge/script.js
@@ -0,0 +1,30 @@
+// Create a variable called sentence. The value of the variable should be a string that contains the words “not” and “bad”.
+// For example, “The movie is not that bad, I like it”.
+
+// Create a variable called wordNot where it’s value is the first appearance (ie. the position) of the substring “not” (from the sentence variable).
+
+// Create a variable called wordBad where it’s value is the first appearance (ie. the position) of the substring “bad” (from the sentence variable).
+
+// If the word “bad” comes after the word “not”, you should replace the whole “not…bad” substring with “good”, then console.log the result.
+// For example, the result here should be : “The movie is good, I like it”
+// If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original sentence.
+
+
+// let sentence = "The movie is not that bad, I like it";
+// let wordBad = sentence.indexOf("bad");
+// console.log(wordBad); //22
+// let wordNot = sentence.indexOf("not");
+// console.log(wordNot); //13
+
+
+
+let sentence = "The movie is not that bad, I like it";
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
+
+if (wordNot < wordBad) {
+  sentence = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+  console.log(sentence); }
+  else{
+    console.log(sentence);
+  }
